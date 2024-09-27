b6b47bb722e572c6fab6ab871c690aa75f39bcc7
Piyush Duggal <duggalpiyush0@gmail.com>
2024-09-28 01:26:13 +0530

    updated

--- dmenu/javascript_project_openner.sh
+++ dmenu/javascript_project_openner.sh
@@ -2,8 +2,16 @@
 
 dirname="$(ls ~/Piyush/Personal-Projects/Javascript -c1 | dmenu -i -l 20 -p 'Select the project')"
 
-if [ -z "$dirname" ]; then
-  exit
-fi
+found=false
 
-xdotool type "nvim ~/Piyush/Personal-Projects/Javascript/$dirname"
+for item in "${array[@]}"; do
+  if [[ "$item" == "$dirname" ]]; then
+    found=true
+    xdotool type "nvim ~/Piyush/Personal-Projects/Javascript/$dirname"
+    break
+  fi
+done
+
+if [[ "$found" == false ]]; then
+  xdotool type "~/Piyush/Personal-Projects/Javascript"
+fi
--- dmenu/python_project_openner.sh
+++ dmenu/python_project_openner.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+array=($(ls ~/Piyush/Personal-Projects/Python -c1))
+
 dirname="$(ls ~/Piyush/Personal-Projects/Python -c1 | dmenu -i -l 20 -p 'Select the project')"
 
-if [ -z "$dirname" ]; then
-  exit
-fi
+found=false
 
-xdotool type "nvim ~/Piyush/Personal-Projects/Python/$dirname"
+for item in "${array[@]}"; do
+  if [[ "$item" == "$dirname" ]]; then
+    found=true
+    xdotool type "nvim ~/Piyush/Personal-Projects/Python/$dirname"
+    break
+  fi
+done
+
+if [[ "$found" == false ]]; then
+  xdotool type "~/Piyush/Personal-Projects/Python"
+fi
